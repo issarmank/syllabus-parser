89b16935346b85956b6e932e162b1d4fe01ada09
Issar Manknojiya <imanknoj@uwo.ca>
2025-10-05 19:35:56 -0400

    variables for railway added in vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,10 +2,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import syllabus
+from app.config import OPENAI_API_KEY
 
 app = FastAPI(title="Syllabus Parser API")
 
-allowed = [
+allowed_origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://syllabus-parser-alpha.vercel.app/",  # replace with your actual Vercel domain
@@ -13,9 +14,10 @@ allowed = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed,
+    allow_origins=allowed_origins,
     allow_methods=["*"],
     allow_headers=["*"],
+    allow_credentials=False,
 )
 
 @app.get("/")
